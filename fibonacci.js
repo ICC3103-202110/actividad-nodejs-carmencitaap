eabd9b44aca472c28c29e8970c35170605a0ef89
carmencitaap <cmavendano@miuandes.cl>
2021-05-02 22:14:58 -0400

    fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,16 @@
+function fib(numero){
+    if (numero == 0 || numero == 1){
+        return numero
+    } 
+    return fib(numero-1) + fib(numero-2)
+}
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+readline.question("Ingresa un número: ", num=> {
+    console.log(fib(num))
+    readline.close()
+})
